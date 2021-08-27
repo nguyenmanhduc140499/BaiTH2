ae7dbc9d1aa3f597a8ab99a9ee3af4ec860fa53b
duc.nguyen <nguyenmanhduc406@gmail.com>
2021-08-27 13:43:59 +0700

    Add project files.

---
+++ BaiTH2/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTH2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetInfoSV( string Ten, string Lop, int Tuoi)
+        {
+            ViewBag.Ten = Ten;
+            ViewBag.Tuoi = Tuoi;
+            ViewBag.Lop = Lop;
+            return View();
+        }
+    }
+}
